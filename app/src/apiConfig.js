9721a6af7b1afa243d9b338ceaa7a60aaaa9bda0
9099MS <9099ms@gmail.com>
2025-09-18 22:45:54 +0900

    3

--- app/src/apiConfig.js
+++ app/src/apiConfig.js
@@ -1,5 +1,5 @@
 const API_BASE_URL = process.env.NODE_ENV === 'production'
-  ? 'https://webtss-1.onrender.com' // 예: https://my-api.com
+  ? process.env.REACT_APP_API_URL
   : 'http://localhost:3001';
 
 export default API_BASE_URL;
